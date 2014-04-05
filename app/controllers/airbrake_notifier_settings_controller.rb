cd1775f570900cceb4175fb3b4c453d54c8813c7
suer <suetsugu.r@gmail.com>
2014-04-05 19:13:04 +0900

    handle parse error

--- app/controllers/airbrake_notifier_settings_controller.rb
+++ app/controllers/airbrake_notifier_settings_controller.rb
@@ -5,11 +5,14 @@ class AirbrakeNotifierSettingsController < ApplicationController
     url = params[:url]
     api_key = params[:api_key]
 
-    save_plugin_settings(url, api_key)
-
     configure_airbrake(url, api_key)
 
+    save_plugin_settings(url, api_key)
+
     flash[:notice] = l(:notice_successful_update)
+    redirect_to :controller => 'settings', :action => 'index', :tab => 'redmine_airbrake_notifier'
+  rescue => e
+    flash[:error] = e.message
     redirect_to :controller => 'settings', :action => 'index', :tab => 'redmine_airbrake_notifier'
   end
 
@@ -19,7 +22,9 @@ class AirbrakeNotifierSettingsController < ApplicationController
   end
 
   def configure_airbrake(url, api_key)
+    return if url.blank? or api_key.blank?
     uri = URI.parse(url)
+    raise l(:airbrake_plugin_message_invalid_url, :url => url) if uri.host.blank? or uri.port.blank?
     Airbrake.configure do |config|
       config.api_key = api_key
       config.host    = uri.host
